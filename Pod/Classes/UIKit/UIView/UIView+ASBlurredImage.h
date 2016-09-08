32c4203626c651103b1538042a91b721bfb3c293
Whirlwind <whirlwindjames@foxmail.com>
2016-09-09 01:03:55 +0800

    public -[UIView as_snapshotImage]

--- Pod/Classes/UIKit/UIView/UIView+ASBlurredImage.h
+++ Pod/Classes/UIKit/UIView/UIView+ASBlurredImage.h
@@ -5,9 +5,10 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (ASBlurredImage)
--(UIImage *)as_blurredImage;
--(UIImage *)as_blurredImageWithDarkEffect;
--(UIImage *)as_blurredImageWithLightEffect;
--(UIImage *)as_blurredImageWithExtraLightEffect;
--(UIImage *)as_blurredImageWithTint:(UIColor*)color;
+- (UIImage *)as_snapshotImage;
+- (UIImage *)as_blurredImage;
+- (UIImage *)as_blurredImageWithDarkEffect;
+- (UIImage *)as_blurredImageWithLightEffect;
+- (UIImage *)as_blurredImageWithExtraLightEffect;
+- (UIImage *)as_blurredImageWithTint:(UIColor*)color;
 @end
